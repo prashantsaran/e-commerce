3eca8b13fb8160e872e9b361fa3ae5f4fe87986b
Prashant <prashantsaransingh@gmail.com>
2023-06-23 18:50:43 +0530

    interface directory created

---
+++ src/app/interface/items.ts
@@ -0,0 +1,12 @@
+export interface Items {
+
+    name: string;
+    color: string;
+    price: number;
+    quantity: number;
+    imageUrl: string;
+    clicked: boolean;
+    totalItems:number;
+    info:any;
+
+}
